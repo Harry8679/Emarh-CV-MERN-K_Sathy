30c54bf128c52757535903d53f3bfd2def860a4e
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-21 04:56:42 +0100

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
         </Routes>
